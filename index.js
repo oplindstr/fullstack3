0b504897df76116bf25c9002cbcd604b952f5403
Olli-Pekka Lindstrom <olli-pekka.lindstrom@helsinki.fi>
2018-02-02 21:53:57 +0200

    herokukorjaus

--- index.js
+++ index.js
@@ -93,6 +93,7 @@ let persons = [
   })
   
 
-const port = 3001
-app.listen(port)
-console.log(`Server running on port ${port}`)
+  const PORT = process.env.PORT || 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
