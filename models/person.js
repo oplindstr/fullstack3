3dd0f8713af8e0c8a850768b3e537db05803e7ae
Olli-Pekka Lindstrom <olli-pekka.lindstrom@helsinki.fi>
2018-02-11 21:08:38 +0200

    Osa 3 melkein loppuun

--- models/person.js
+++ models/person.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
 
-const url = 'mongodb://fullstack:fullpassword@ds121088.mlab.com:21088/opmongo'
+if ( process.env.NODE_ENV !== 'production' ) {
+    require('dotenv').config()
+  }
+  
+const url = process.env.MONGODB_URI
 
 mongoose.connect(url)
 mongoose.Promise = global.Promise;
